6f3ef0a5f3a428d178dbffbb924e895495c54f0a
vvasylykha <vvasylykha@unitedthinkers.com>
2023-02-13 20:23:37 +0200

    homework 1

--- test.js
+++ test.js
@@ -1,19 +1,7 @@
-{
-  "name": "programmingtech2023",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/PavloYakhontov/ProgrammingTech2023.git"
-  },
-  "author": "",
-  "license": "ISC",
-  "bugs": {
-    "url": "https://github.com/PavloYakhontov/ProgrammingTech2023/issues"
-  },
-  "homepage": "https://github.com/PavloYakhontov/ProgrammingTech2023#readme"
-}
+let name = "Ілля";
+
+alert(`привіт ${1}`); // 1
+
+alert(`привіт ${"name"}`); // name
+
+alert(`привіт ${name}`); // Ілля 
