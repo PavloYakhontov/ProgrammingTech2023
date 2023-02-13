6fa2eaedfb4207477f0dc3427f49102ea054d1ce
Oleksii Holodniak <holodniakwork@gmail.com>
2023-02-13 23:32:33 +0200

    homework

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 console.log("Hello World!");
 
-let name = "Олексій";
+let name = "Бік-Мек";
 
-alert(`привіт ${1}`);
+alert(`привіт ${1}`); // привіт 1
 
-alert(`привіт ${"name"}`);
+alert(`привіт ${"name"}`); // привіт name
 
-alert(`привіт ${name}`);
+alert(`привіт ${name}`); // привіт, Бік-Мек
