1a9d101caab185b81c54d9d2521c56f332904b4d
Numed <ruschinets09@gmail.com>
2023-02-13 14:28:33 +0200

    finish first homework

--- js/test.js
+++ js/test.js
@@ -1,3 +1,11 @@
-"use strict"
+"use strict";
 
 console.log("Hello World!");
+
+let name = "Ілля";
+
+alert(`привіт ${1}`); // привіт 1
+
+alert(`привіт ${"name"}`); // привіт name
+
+alert(`привіт ${name}`); // привіт, Ілля
