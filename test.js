2cbbbef1bfe874902a714345f3176a379eedc565
YuriyDz <dziapko.yurii@student.uzhnu.edu.ua>
2023-02-14 20:40:32 +0200

    додає код

--- test.js
+++ test.js
@@ -1,19 +1,3 @@
-{
-  "name": "programmingtech2023",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/PavloYakhontov/ProgrammingTech2023.git"
-  },
-  "author": "",
-  "license": "ISC",
-  "bugs": {
-    "url": "https://github.com/PavloYakhontov/ProgrammingTech2023/issues"
-  },
-  "homepage": "https://github.com/PavloYakhontov/ProgrammingTech2023#readme"
-}
+console.log("Hello world!");
+  
+
