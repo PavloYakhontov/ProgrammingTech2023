2b457e9c130b350404ceb2ee941717b8f6989695
Mirzilla <vadiimii2004e@gmail.com>
2023-02-19 17:02:51 +0200

    Its task1 for JS

---
+++ Task1.js
@@ -0,0 +1,40 @@
+// 2.10
+// 1
+// if ("0") {
+//     alert('Hello');
+// }//Так буде виведено а чому та по пріколу)))))
+
+// // 2
+// let value = prompt('Яка "офіційна" назва JavaScript?', '');
+
+//     if (value == 'ECMAScript') {
+//       alert('Правильно!');
+//     } else {
+//       alert('Ви не знаєте? ECMAScript!');
+//     }
+// 3
+let result, a, b;
+    a + b < 4 ? result = "Нижче" : "Вище"
+
+// 4 
+let login;
+let message = (login == 'Працівник') ? 'Привіт' :
+  (login == 'Директор') ? 'Вітаю' :
+  (login == '') ? 'Немає логіну' :
+  '';
+
+
+// 2.13
+
+// 1
+// let i = 3;
+
+// while (i) {
+//   alert( i-- );
+// }
+// Will be 3
+
+// 3
+for(let i = 2; i <= 10; i++) {
+    console.log(i);
+}
