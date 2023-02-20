a2bbfa22e293b321999b1b462da2ee72e84b5926
DESKTOP-FK2D82I\SaKe <sasha.g360@gmail.com>
2023-02-20 04:15:20 +0200

    added Task 1

---
+++ module_1/Task_1.js
@@ -0,0 +1,16 @@
+// Робота зі змінними
+
+let admin
+let Name = "Іван"
+admin = Name
+alert(admin)
+
+// Лапки у рядках
+
+let name = "Ілля";
+
+alert( `привіт ${1}` ); // привіт 1
+
+alert( `привіт ${"name"}` ); // привіт name 
+
+alert( `привіт ${name}` ); // привіт Ілля
