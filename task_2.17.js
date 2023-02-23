dc7e08123432a606d9443f875297b72d2fa11859
Lekhman Ann <lekhman.hanna@student.uzhnu.edu.ua>
2023-02-23 20:42:10 +0200

    second lesson

---
+++ task_2.17.js
@@ -0,0 +1,10 @@
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Ви згодні?",
+    () => alert("Ви погодились."),
+    () => alert("Ви скасували виконання.")
+);
---
+++ test.js
@@ -0,0 +1,11 @@
+"use strict"
+
+console.log('Hello world !!!');
+
+let name = "Ілля";
+
+alert(привіт ${1}); // привіт 1
+
+alert(привіт ${'name'}); // привіт name
+
+alert(привіт ${name}); // привіт, Ілля
