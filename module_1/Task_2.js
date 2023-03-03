ef2bc82f341b166233d2d000f9d7d54db4d1c045
DESKTOP-FK2D82I\SaKe <sasha.g360@gmail.com>
2023-03-03 03:47:22 +0200

    edited Task_2

--- module_1/Task_2.js
+++ module_1/Task_2.js
@@ -5,4 +5,48 @@ let a = 1, b = 1;
 let c = ++a; // 2
 let d = b++; // 1
 
+//Оператори порівняння
 
+5 > 4 //true
+"ананас" > "яблуко" //false
+"2" > "12" //true
+undefined == null //true
+undefined === null //false
+null == "\n0\n" //false
+null === +"\n0\n" //false
+
+//Назва JavaScript
+let value = prompt('Яка "офіційна" назва JavaScript?', '');
+
+if (value == 'ECMAScript') {
+    alert('Правильно!');
+} else {
+    alert('Ви не знаєте? ECMAScript!');
+}
+
+//Перепишіть 'if..else' на '?'
+let message = (login == 'Працівник') ? 'Привіт' :
+              (login == 'Директор') ? 'Вітаю' :
+              (login == '') ? 'Немає логіну' :'';
+
+
+//  Перевірте логін             
+let userName = prompt('Хто там?', '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Пароль?', '');
+
+  if (pass === 'Господар') {
+    alert( 'Ласкаво просимо!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Скасовано' );
+  } else {
+    alert( 'Неправильний пароль' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Скасовано' );
+} else {
+  alert( 'Я вас не знаю' );
+}
